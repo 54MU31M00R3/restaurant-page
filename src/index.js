22d07c41862c2ffc875d8dc83e35560d1abcb8f5
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2025-07-23 19:06:27 -0400

    Create module for about us page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './styles.css';
 
 import { homePage } from './pages/home.js';
 import { menuPage } from './pages/menu.js';
+import { aboutPage } from './pages/about.js';
 
 // document.body.appendChild(homePage.getPage());
 // document.body.appendChild(menuPage.getPage());
+// document.body.appendChild(aboutPage.getPage());
---
+++ src/pages/about.js
@@ -0,0 +1,51 @@
+import gelatoBobaUrl from '../assets/images/gelato_and_tea.jpg';
+
+const aboutPage = (function () {
+    const page = document.createElement("div");
+    page.id = "content"
+
+    const getHeadline = () => {
+        const headline = document.createElement("div");
+        headline.id = "headline";
+        headline.textContent = "Where did we go?";
+
+        return headline;
+    }
+
+    const getAboutImage = () => {
+        const aboutImage = document.createElement("img");
+        aboutImage.id = "about-image";
+        aboutImage.src = gelatoBobaUrl;
+
+        return aboutImage
+    }
+
+    const getAboutDesc = () => {
+        const aboutDesc = document.createElement("div");
+        aboutDesc.id = "about-desc";
+        aboutDesc.textContent = `After a cozy hiatus, our Asian infusion gelato and boba shop is
+        back—just for the summer! Dive into handcrafted gelato with bold,
+        nostalgic flavours like matcha, black sesame, and Thai tea, paired with
+        refreshing boba drinks made fresh daily. A seasonal pop-up you won’t
+        want to miss!`;
+
+        return aboutDesc;
+    }
+
+    const getPage = () => {
+        const headline = getHeadline();
+        page.appendChild(headline);
+
+        const aboutImage = getAboutImage();
+        page.appendChild(aboutImage);
+
+        const aboutDesc = getAboutDesc();
+        page.appendChild(aboutDesc);
+
+        return page;
+    }
+    
+    return { getPage };
+})();
+
+export { aboutPage };
