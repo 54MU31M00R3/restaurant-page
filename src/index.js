3aa8f3ae6eb8abf7542b1c54bd2116026b304f31
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2025-07-23 18:37:32 -0400

    Create module for menu page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles.css';
 
 import { homePage } from './pages/home.js';
+import { menuPage } from './pages/menu.js';
 
 // document.body.appendChild(homePage.getPage());
+document.body.appendChild(menuPage.getPage());
---
+++ src/pages/menu.js
@@ -0,0 +1,84 @@
+import greenTeaUrl from '../assets/images/green_gelato.jpg';
+import milkTeaUrl from '../assets/images/milk_tea_gelato.jpeg';
+
+const menuPage = (function () {
+    const page = document.createElement("div");
+    page.id = "content";
+
+    let gelato = [
+        {
+            flavour: "GREEN TEA",
+            price: "$5",
+            ingredients: "Infused with matcha green tea",
+            image: greenTeaUrl,
+        },
+        {
+            flavour: "MILK TEA + BOBA",
+            price: "$7",
+            ingredients: "Infused with loose-leaf black tea",
+            image: milkTeaUrl,
+        },
+    ]
+
+    const getHeadline = () => {
+        const headline = document.createElement("div");
+        headline.id = "headline";
+        headline.textContent = "Gelato Flavours";
+        return headline;
+    }
+
+    const getMenu = () => {
+        const menu = document.createElement("div");
+        menu.id = "menu-container";
+
+        gelato.forEach((item) => {
+            const menuItem = document.createElement("div");
+            menuItem.className = "menu-item"
+            menu.appendChild(menuItem);
+
+            const itemDesc = document.createElement("div");
+            itemDesc.className = "item-desc";
+            menuItem.appendChild(itemDesc);
+
+            const itemHeader = document.createElement("div");
+            itemHeader.className = "item-header";
+            itemDesc.appendChild(itemHeader);
+
+            const itemName = document.createElement("div");
+            itemName.className = "item-name";
+            itemName.textContent = item.flavour;
+            itemHeader.appendChild(itemName);
+
+            const itemPrice = document.createElement("div");
+            itemPrice.className = "item-price";
+            itemPrice.textContent = item.price;
+            itemHeader.appendChild(itemPrice);
+
+            const itemIngredients = document.createElement("div");
+            itemIngredients.className = "item-ingredients";
+            itemIngredients.textContent = item.ingredients;
+            itemDesc.appendChild(itemIngredients);
+
+            const itemImage = document.createElement("img");
+            itemImage.className = "item-image"
+            itemImage.src = item.image;
+            menuItem.appendChild(itemImage);
+
+            menu.appendChild(menuItem);
+        });
+        return menu;
+    }
+    const getPage = () => {
+        const headline = getHeadline();
+        page.appendChild(headline);
+
+        const menu = getMenu();
+        page.appendChild(menu);
+
+        return page;
+    }
+
+    return { getPage };
+})();
+
+export { menuPage };
